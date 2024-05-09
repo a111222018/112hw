ee038d22129e2b95fb6bddefc3ccaf54bc81e691
a111222018 <a111222018@mail.shu.edu.tw>
2024-05-09 11:22:19 +0800

    2024/5/9/fix

--- foodOrder/app/src/main/java/com/example/foodorder/MainActivity.java
+++ foodOrder/app/src/main/java/com/example/foodorder/MainActivity.java
@@ -13,12 +13,12 @@ public class MainActivity extends AppCompatActivity implements CheckBox.OnChecke
 
 
     private int count=0;
-    private ImageView img, img2, img3, img4;
-    private CheckBox chk1, chk2, chk3, chk4;
-    private int[] chkID={R.id.chk1,R.id.chk2,R.id.chk3, R.id.chk4};
+    private ImageView img, img2, img3, img4,img5;
+    private CheckBox chk1, chk2, chk3, chk4,chk5;
+    private int[] chkID={R.id.chk1,R.id.chk2,R.id.chk3, R.id.chk4,R.id.chk5};
 
     private TextView show;
-    private int[] imgID={R.id.img1,R.id.img2,R.id.img3, R.id.img4};
+    private int[] imgID={R.id.img1,R.id.img2,R.id.img3, R.id.img4,R.id.img5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +33,12 @@ public class MainActivity extends AppCompatActivity implements CheckBox.OnChecke
         chk2=(CheckBox) findViewById(R.id.chk2);
         chk3=(CheckBox) findViewById(R.id.chk3);
         chk4=(CheckBox) findViewById(R.id.chk4);
-
+        chk5=(CheckBox) findViewById(R.id.chk5);
         chk1.setOnCheckedChangeListener(this);
         chk2.setOnCheckedChangeListener(this);
         chk3.setOnCheckedChangeListener(this);
         chk4.setOnCheckedChangeListener(this);
+        chk5.setOnCheckedChangeListener(this);
 
     }
 
